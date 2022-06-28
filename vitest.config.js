71a407f33c8ee17ac605a3ad604ae6b6af419154
remiconnesson <remiconnesson2@gmail.com>
2022-06-28 17:38:02 +0000

    OK: vitest with auto imports of components

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,18 @@
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 export default {
 	plugins: [
     vue(),
     AutoImport({
         imports: [
-    // presets
-    'vue']
-    })
+          'vue'
+        ],
+    }),
+    Components({
+      dirs: ['./components']
+    }),
   ],
 	test: {
     globals: true,
